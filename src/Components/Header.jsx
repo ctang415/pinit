9e64cbf27877bbb9071cc47e4360fb01de2afee6
ctang415 <carrietang117@gmail.com>
2025-02-06 20:06:08 -0500

    Created initial mockup for header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,14 @@
+import SearchBar from "./SearchBar"
 
 const Header = () => {
 
     return (
-        <div className="p-4 bg-slate-400">
-            I am a header!
+        <div className="p-6 flex justify-between border-b-2 border-stone-800 items-center">
+            <div className="mx-8">PinIt</div>
+            <div>
+                <SearchBar/>
+            </div>
+            <div className="mx-8">LOGIN SETTINGS</div>
         </div>
     )
 }
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,7 +1,8 @@
 
-
 const SearchBar = () => {
-    
+    return (
+        <input type="text" className="rounded-lg p-2 bg-zinc-900 min-w-sm focus:outline-none"></input>
+    )
 }
 
 export default SearchBar
