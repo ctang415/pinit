a819521333ea938ada72f47614338a392a522ffa
ctang415 <carrietang117@gmail.com>
2025-02-07 19:32:35 -0500

    Stylized button component

---
+++ src/Components/Button.jsx
@@ -0,0 +1,7 @@
+const Button = ({text}) => {
+    return (
+        <button className="py-2 px-4 rounded-3xl bg-white text-black hover:cursor-pointer hover:bg-zinc-400">{text}</button>
+    )
+}
+
+export default Button
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,20 @@
+import Button from "./Button"
 import SearchBar from "./SearchBar"
 
 const Header = () => {
 
     return (
         <div className="p-6 flex justify-between border-b-2 border-stone-800 items-center">
-            <div className="mx-8">PinIt</div>
+            <div className="mx-2">
+                <a className="text-3xl">PinIt</a>
+            </div>
             <div>
                 <SearchBar/>
             </div>
-            <div className="mx-8">LOGIN SETTINGS</div>
+            <div className="mx-2 flex gap-4">
+                <Button text={"Login"}/>
+                <Button text={"Sign Up"}/>
+            </div>
         </div>
     )
 }
