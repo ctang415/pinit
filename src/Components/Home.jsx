cb69b511055df1e737246beafccba84fb6479e71
ctang415 <carrietang117@gmail.com>
2025-02-11 23:03:39 -0500

    Create navbar for tags with tag component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,28 @@
 import Header from "./Header"
+import Tag from "./Tag"
 
 const Home = () => {
     return (
         <>
             <Header/>
+            <nav className="p-2">
+                <ul className="flex justify-evenly gap-4">
+                    <Tag text={"Tag"}/>
+                    <Tag text={"Tag"}/>
+                    <Tag text={"Tag"}/>
+                    <Tag text={"Tag"}/>
+                    <Tag text={"Tag"}/>
+                    <Tag text={"Tag"}/>
+                    <Tag text={"Tag"}/>
+                    <Tag text={"Tag"}/>
+                </ul>
+            </nav>
+            <div>
+                <h1>Collections</h1>
+            </div>
+            <div>
+                <h1>Images</h1>
+            </div>
         </>
     )
 }
---
+++ src/Components/Tag.jsx
@@ -0,0 +1,8 @@
+
+const Tag = ({text}) => {
+    return (
+        <li className="text-zinc-400 text-md hover:cursor-pointer hover:text-white">{text}</li>
+    )
+}
+
+export default Tag
