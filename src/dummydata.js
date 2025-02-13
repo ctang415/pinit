b253c87d766cc7edf0ee162035d5f116cee7f763
ctang415 <carrietang117@gmail.com>
2025-02-13 17:04:54 -0500

    Updated dummy data for easier cross referencing

--- src/dummydata.js
+++ src/dummydata.js
@@ -3,73 +3,132 @@ let dummyAccounts = [
     "password": "testpass"}
 ]
 
-let dummyData = [
-    {
-        "collections": [
+let dummyCollections = [
             {
+                "id": 1,
                 "title": "fashion inspo",
                 "creator": "v0gue",
-                "link": "",
                 "imagesInCollection": [
-                    {"imageLink": "https://i.pinimg.com/736x/f0/16/87/f01687053e7ac8837ca1964390a8dd62.jpg"},
-                    {"imageLink": "https://offduty.in/cdn/shop/products/IMG-20221013-WA0018_1_1080x.jpg?v=1721927302"},
-                    {"imageLink": "https://magnoliaoh.com/cdn/shop/files/2ce3d1d923c90cdaa5a27748404a9deb52d5b8475d397cad5d00989c0c80ad9f.jpg"},
-                    {"imageLink": "https://i5.walmartimages.com/seo/Women-s-Summer-Casual-Sleeveless-Crew-Neck-Smocked-High-Waist-Flowy-Tiered-A-Line-Midi-Dress-Womens-Long-Sleeve-Women-Dresses-Length_f69f6ac5-4b22-4ffc-aeef-f6bc8774554b.b820699983db5019afde867e3575684a.jpeg"}
+                    {"imageId": 4},
+                    {"imageId": 9},
+                    {"imageId": 12},
+                    {"imageId": 14}
                 ]
             },
             {
+                "id": 2,
                 "title": "fall season mood",
                 "creator": "ally.b",
-                "link": "",
                 "imagesInCollection": [
-                    {"imageLink": "https://images.squarespace-cdn.com/content/v1/592c627315d5db2bc1ca49ec/1598997282660-LOS0NM14H6H8BXBVC0MV/falltrain.jpg"},
-                    {"imageLink": "https://i0.wp.com/mcconnellmarketing.com/wp-content/uploads/2022/10/4-Small-Towns-That-Give-Off-the-Perfect-Fall-Vibes-inpost.jpg"},
-                    {"imageLink": "https://hemmingandwills.co.uk/cdn/shop/articles/create_earthy_vibes_in_home_in_autumn_1600x@2x.jpg"},
-                    {"imageLink": "https://img.acolorstory.com/uploads/2019/10/A-Color-Story-Fall-Vibes-6-1-1869x2800.jpg"}
+                    {"imageId": 15},
+                    {"imageId": 16},
+                    {"imageId": 6},
+                    {"imageId": 2},
                 ]
             },
             {
+                "id": 3,
                 "title": "cities to visit",
                 "creator": "wanderlustt",
-                "link": "",
                 "imagesInCollection": [
-                    {"imageLink": "https://media.cntravellerme.com/photos/64ac6f15ecefc2b0f9a1dff9/master/w_2580%2Cc_limit/Dubrovnik-Croatia-july23-577007721.jpg"},
-                    {"imageLink": "https://media.cntravellerme.com/photos/647f613dd49d9911e8a60b09/master/w_2580%2Cc_limit/BestCitiesInTheWorld-2020-Ljubljana%2C%2520Slovenia-GettyImages-495290964.jpg"},
-                    {"imageLink": "https://media.cntravellerme.com/photos/647f6126d49d9911e8a60b03/16:9/w_4240,h_2385,c_limit/BestCitiesInTheWorld-2020-Bergen,%20Norway-GettyImages-1169113458.jpg"},
-                    {"imageLink": "https://media.cntravellerme.com/photos/64ac6f1451562a659243e254/master/w_2580%2Cc_limit/Venice-Italy-july23-535829619.jpg"}
+                    {"imageId": 10},
+                    {"imageId": 3},
+                    {"imageId": 1},
+                    {"imageId": 8}
                 ]
             },
             {
+                "id": 4,
                 "title": "pockets of joy",
                 "creator": "pursuitofhappiness",
-                "link": "",
                 "imagesInCollection": [
-                    {"imageLink": "https://images.fineartamerica.com/images-medium-large-5/peaceful-starry-night-yoshiki-nakamura.jpg"},
-                    {"imageLink": "https://grca.org/wp-content/uploads/2015/08/shutterstock_123176032.jpg"},
-                    {"imageLink": "https://pics.craiyon.com/2023-07-15/9f4bacc2f7ea463ca9f07d8cdca50426.webp"},
-                    {"imageLink": "https://critterstop.com/nitropack_static/wuKUaxqsmqLhAYRpfOaXVBNknEpAdAKp/assets/images/optimized/rev-d27ea91/critterstop.com/wp-content/uploads/2024/05/01-why-are-animals-so-cute-1024x538.webp"}
+                    {"imageId": 13},
+                    {"imageId": 5},
+                    {"imageId": 11},
+                    {"imageId": 7},
                 ]
             }
-        ], 
-        "images": [
-            {"link": "https://media.cntravellerme.com/photos/647f6126d49d9911e8a60b03/16:9/w_4240,h_2385,c_limit/BestCitiesInTheWorld-2020-Bergen,%20Norway-GettyImages-1169113458.jpg", 
-            "tags": ["travel", "city", "norway", "mountains"]
-            },
-            {"link": "https://img.acolorstory.com/uploads/2019/10/A-Color-Story-Fall-Vibes-6-1-1869x2800.jpg", 
-            "tags": ["autumn", "fall", "seasonal", "orange", "trees", "nature", "peaceful"]
-            },
-            {"link": "https://media.cntravellerme.com/photos/647f613dd49d9911e8a60b09/master/w_2580%2Cc_limit/BestCitiesInTheWorld-2020-Ljubljana%2C%2520Slovenia-GettyImages-495290964.jpg", 
-            "tags": ["travel", "city", "snowy", "slovenia"]
-            },
-            {"link": "https://i.pinimg.com/736x/f0/16/87/f01687053e7ac8837ca1964390a8dd62.jpg", 
-            "tags": ["cardigan", "casual", "grey", "sweater"]
-            },
-            {"link": "https://grca.org/wp-content/uploads/2015/08/shutterstock_123176032.jpg", 
-            "tags": ["dogs", "animals", "puppies", "golden retriever", "cute"]
-            },
-            {"link": "https://hemmingandwills.co.uk/cdn/shop/articles/create_earthy_vibes_in_home_in_autumn_1600x@2x.jpg", 
-            "tags": ["pumpkin", "holiday", "home decor", "leaves", "autumn", "fall"]
-            },
-        ]
-    }
 ]
+
+let dummyImages = [
+            {
+                "link": "https://media.cntravellerme.com/photos/647f6126d49d9911e8a60b03/16:9/w_4240,h_2385,c_limit/BestCitiesInTheWorld-2020-Bergen,%20Norway-GettyImages-1169113458.jpg", 
+                "tags": ["travel", "city", "norway", "mountains"],
+                "id": 1
+            },
+            {
+                "link": "https://img.acolorstory.com/uploads/2019/10/A-Color-Story-Fall-Vibes-6-1-1869x2800.jpg", 
+                "tags": ["autumn", "fall", "seasonal", "orange", "trees", "nature", "peaceful"],
+                "id": 2
+            },
+            {
+                "link": "https://media.cntravellerme.com/photos/647f613dd49d9911e8a60b09/master/w_2580%2Cc_limit/BestCitiesInTheWorld-2020-Ljubljana%2C%2520Slovenia-GettyImages-495290964.jpg", 
+                "tags": ["travel", "city", "snowy", "slovenia"],
+                "id": 3
+            },
+            {
+                "link": "https://i.pinimg.com/736x/f0/16/87/f01687053e7ac8837ca1964390a8dd62.jpg", 
+                "tags": ["cardigan", "casual", "grey", "sweater"],
+                "id": 4
+            },
+            {
+                "link": "https://grca.org/wp-content/uploads/2015/08/shutterstock_123176032.jpg", 
+                "tags": ["dogs", "animals", "puppies", "golden retriever", "cute"],
+                "id": 5
+            },
+            {
+                "link": "https://hemmingandwills.co.uk/cdn/shop/articles/create_earthy_vibes_in_home_in_autumn_1600x@2x.jpg", 
+                "tags": ["pumpkin", "holiday", "home decor", "leaves", "autumn", "fall"],
+                "id": 6
+            },
+            {
+                "link": "https://critterstop.com/nitropack_static/wuKUaxqsmqLhAYRpfOaXVBNknEpAdAKp/assets/images/optimized/rev-d27ea91/critterstop.com/wp-content/uploads/2024/05/01-why-are-animals-so-cute-1024x538.webp",
+                "tags": [],
+                "id": 7
+            },
+            {
+                "link": "https://media.cntravellerme.com/photos/64ac6f1451562a659243e254/master/w_2580%2Cc_limit/Venice-Italy-july23-535829619.jpg",
+                "tags": [],
+                "id": 8
+            }, 
+            {
+                "link": "https://offduty.in/cdn/shop/products/IMG-20221013-WA0018_1_1080x.jpg?v=1721927302",
+                "tags": [],
+                "id": 9
+            },
+            {
+                "link": "https://media.cntravellerme.com/photos/64ac6f15ecefc2b0f9a1dff9/master/w_2580%2Cc_limit/Dubrovnik-Croatia-july23-577007721.jpg",
+                "tags": [],
+                "id": 10
+            },
+            {
+                "link": "https://pics.craiyon.com/2023-07-15/9f4bacc2f7ea463ca9f07d8cdca50426.webp",
+                "tags": [],
+                "id": 11
+            }, 
+            {
+                "link": "https://magnoliaoh.com/cdn/shop/files/2ce3d1d923c90cdaa5a27748404a9deb52d5b8475d397cad5d00989c0c80ad9f.jpg",
+                "tags": [],
+                "id": 12
+            },
+            {
+                "link": "https://images.fineartamerica.com/images-medium-large-5/peaceful-starry-night-yoshiki-nakamura.jpg",
+                "tags": [],
+                "id": 13
+            },
+            {
+                "link": "https://i5.walmartimages.com/seo/Women-s-Summer-Casual-Sleeveless-Crew-Neck-Smocked-High-Waist-Flowy-Tiered-A-Line-Midi-Dress-Womens-Long-Sleeve-Women-Dresses-Length_f69f6ac5-4b22-4ffc-aeef-f6bc8774554b.b820699983db5019afde867e3575684a.jpeg",
+                "tags": [],
+                "id": 14
+            },
+            {
+                "link": "https://images.squarespace-cdn.com/content/v1/592c627315d5db2bc1ca49ec/1598997282660-LOS0NM14H6H8BXBVC0MV/falltrain.jpg",
+                "tags": [],
+                "id": 15
+            }, 
+            {
+                "link": "https://i0.wp.com/mcconnellmarketing.com/wp-content/uploads/2022/10/4-Small-Towns-That-Give-Off-the-Perfect-Fall-Vibes-inpost.jpg",
+                "tags": [],
+                "id": 16
+            }
+        ]
